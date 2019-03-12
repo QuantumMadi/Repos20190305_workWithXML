7843df02ea7191d61612c1dd20dd508f844901fa
Madi <45874448+QuantumMadi@users.noreply.github.com>
2019-03-12 23:16:47 +0600

    1

---
+++ theory/HabrHabr/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabrHabr
+{
+   
+    public class Item
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public DateTime PubDate { get; set; }
+        public string Description { get; set; }
+    }
+}
