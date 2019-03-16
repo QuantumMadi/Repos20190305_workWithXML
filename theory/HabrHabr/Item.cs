339439c54b6ad6cf97dc964dd29dd63740475985
QuantumMadi <45874448+QuantumMadi@users.noreply.github.com>
2019-03-16 16:39:37 +0600

    habr habr

--- theory/HabrHabr/Item.cs
+++ theory/HabrHabr/Item.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace HabrHabr
 {
-   
+   [Serializable]
     public class Item
     {
         public string Title { get; set; }
         public string Link { get; set; }
-        public DateTime PubDate { get; set; }
         public string Description { get; set; }
+        public string PubDate { get; set; }
+        //public Item(string title, string link, string description, DateTime pubDate)
+        //{
+        //    Title = title;
+        //    Link = link;
+        //    Description = description;
+        //    PubDate = pubDate;
+        //}
+
+        public Item() { }
     }
 }
--- theory/HabrHabr/Program.cs
+++ theory/HabrHabr/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
 
 namespace HabrHabr
 {
@@ -10,7 +13,45 @@ namespace HabrHabr
     {
         static void Main(string[] args)
         {
+            List<Item> items = new List<Item>();
 
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Item>));
+
+            var xRoot = DownLoadData();
+          
+            foreach(XmlNode xnode in xRoot)
+            {
+               
+
+                foreach (XmlNode habrItems in xnode.ChildNodes) {
+                    Item item = new Item();
+                    if (habrItems.Name == "item")
+                    {
+                        foreach(XmlNode child in habrItems.ChildNodes)
+                        {
+                            if (child.Name == "title") item.Title = child.InnerText;
+                            if (child.Name == "link") item.Link = child.InnerText;
+                            if (child.Name == "description") item.Description = child.InnerText;
+                            if (child.Name == "pubDate") item.PubDate = child.InnerText;
+                        }                     
+                    }
+                    items.Add(item);
+                }
+              
+                
+            }
+            Console.ReadLine();
+        }
+
+        public static XmlElement DownLoadData()
+        {           
+            WebClient webClient = new WebClient();
+            var xmlFile = Encoding.UTF8.GetString(webClient.DownloadData("https://habrahabr.ru/rss/interesting/"));
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlFile);
+            XmlElement xRoot = xmlDoc.DocumentElement;
+
+            return xRoot;
         }
     }
 }
