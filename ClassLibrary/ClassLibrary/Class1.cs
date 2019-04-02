bb23f737a2ab9ef48463f522a0a928f07e66acc4
Madi <45874448+QuantumMadi@users.noreply.github.com>
2019-04-02 11:55:36 +0600

    2

---
+++ ClassLibrary/ClassLibrary/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class SmsService
+    {
+        public string Message { get; set; }
+        public SmsService(string message)
+        {
+            Message = message;
+        }
+        public void SendMessage(string message, string to)
+        {
+            Console.Writeline($"SMS:{message} for {to}");
+        }
+    }
+}
